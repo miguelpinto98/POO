e56fefe4640616e4debc658b70abed5d8b515ff7
Pedro Lima <pedrolima.0000@gmail.com>
2013-05-06 20:04:19 +0100

    alteraçao nos construtores

--- Circuito.java
+++ Circuito.java
@@ -7,12 +7,56 @@
  *
  * @author griffin
  */
-import java.sql.Time;
+
 public class Circuito {
     private int distancia,nvoltas;
     private Time tpc1,tpc2,tgt,tsc,trecord,tdesviom,tboxes;
     private Piloto recordista;
 
+    //construtores 
+        public Circuito(int distancia, int nvoltas, Time tpc1, Time tpc2, Time tgt, Time tsc, Time trecord, Time tdesviom, Time tboxes, Piloto recordista) {
+        this.distancia = distancia;
+        this.nvoltas = nvoltas;
+        this.tpc1 = tpc1;
+        this.tpc2 = tpc2;
+        this.tgt = tgt;
+        this.tsc = tsc;
+        this.trecord = trecord;
+        this.tdesviom = tdesviom;
+        this.tboxes = tboxes;
+        this.recordista = recordista;
+    }
+
+    public Circuito() {
+         this.distancia = 0;
+        this.nvoltas = 0;
+        this.tpc1 = new Time();
+        this.tpc2 = new Time();
+        this.tgt = new Time();
+        this.tsc = new Time();
+        this.trecord = new Time();
+        this.tdesviom = new Time();
+        this.tboxes = new Time();
+        this.recordista = new Piloto();
+        
+    }
+
+    public Circuito(Circuito c) {
+         this.distancia = c.getDistancia();
+        this.nvoltas = c.getNvoltas();
+        this.tpc1 = c.getTpc1();
+        this.tpc2 = c.getTpc2();
+        this.tgt = c.getTgt();
+        this.tsc = c.getTsc();
+        this.trecord = c.getTrecord();
+        this.tdesviom = c.getTdesviom();
+        this.tboxes = c.getTboxes();
+        this.recordista = c.getRecordista().clone();
+        
+    }
+
+    
+    //get 
     public int getDistancia() {
         return distancia;
     }
@@ -53,6 +97,8 @@ public class Circuito {
         return recordista;
     }
 
+    
+    //set
     public void setDistancia(int distancia) {
         this.distancia = distancia;
     }
@@ -93,51 +139,11 @@ public class Circuito {
         this.recordista = recordista;
     }
 
-    @Override
+    
     public String toString() {
         return "Circuito{" + "distancia=" + distancia + ", nvoltas=" + nvoltas + ", tpc1=" + tpc1 + ", tpc2=" + tpc2 + ", tgt=" + tgt + ", tsc=" + tsc + ", trecord=" + trecord + ", tdesviom=" + tdesviom + ", tboxes=" + tboxes + ", recordista=" + recordista + '}';
     }
 
-    public Circuito(int distancia, int nvoltas, Time tpc1, Time tpc2, Time tgt, Time tsc, Time trecord, Time tdesviom, Time tboxes, Piloto recordista) {
-        this.distancia = distancia;
-        this.nvoltas = nvoltas;
-        this.tpc1 = tpc1;
-        this.tpc2 = tpc2;
-        this.tgt = tgt;
-        this.tsc = tsc;
-        this.trecord = trecord;
-        this.tdesviom = tdesviom;
-        this.tboxes = tboxes;
-        this.recordista = recordista;
-    }
-
-    public Circuito() {
-         this.distancia = 0;
-        this.nvoltas = 0;
-        this.tpc1 = new Time();
-        this.tpc2 = new Time;
-        this.tgt = new Time();
-        this.tsc = new Time();
-        this.trecord = new Time();
-        this.tdesviom = new Time();
-        this.tboxes = new Time();
-        this.recordista = new Piloto();
-        
-    }
-
-    public Circuito(Circuito c) {
-         this.distancia = c.getDistancia();
-        this.nvoltas = c.getNvoltas();
-        this.tpc1 = c.getTpc1();
-        this.tpc2 = c.getTpc2();
-        this.tgt = c.getTgt();
-        this.tsc = c.getTsc();
-        this.trecord = c.getTrecord();
-        this.tdesviom = c.getTdesviom();
-        this.tboxes = c.getTboxes();
-        this.recordista = c.getRecordista().clone();
-        
-    }
 
     
     
