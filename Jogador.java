258cb09a37adb4fbdf2865630d4aa128254cb94f
Pedro Lima <pedrolima.0000@gmail.com>
2013-05-07 18:49:14 +0100

    jogador

---
+++ Jogador.java
@@ -0,0 +1,16 @@
+public class Jogador {
+
+private String nome,morada; 
+private ArrayList<Aposta> adecorrer, hist;  //o histórico de apostas realizadoe as apostas em vigor
+private double di,dg,dc; // a conta corrente, investimento e ganhos
+
+
+//get 
+public String getNome(){
+return nome;
+}
+public String getMorada(){
+return morada;
+}
+public ArrayList<Aposta>
+}
