533de001b6c852bcb4f92b89ec885017f9566c88
Pedro Lima <pedrolima.0000@gmail.com>
2013-05-11 18:13:45 +0100

    comparadores para o TIME

---
+++ Comparator0Time.java
@@ -0,0 +1,23 @@
+ import java.util.Comparator;
+
+ public class Comparator1Time implements Comparator < Time > {
+
+ 	public int compareTo(Time p) {
+ 		if (this.getHoras() < p.getHoras())
+ 			return 1;
+ 		if (this.getHoras() > p.getHoras())
+ 			return -1;
+ 		if (this.getMinutos() < p.getMinutos())
+ 			return 1;
+ 		if (this.getMinutos() > getMinutos())
+ 			return -1;
+ 		if (this.getSegundo() < p.getSegundo())
+ 			return 1;
+ 		if (this.getSegundo() > p.getSegundo())
+ 			return -1;
+ 		else
+ 			return 0;
+
+ 	}
+
+ }
