13f97300fa57981a423ae2a1cdcdb9f154d1913b
Miguel Pinto <mcpinto98@gmail.com>
2013-05-17 18:39:31 +0100

    sc update

--- SC.java
+++ SC.java
@@ -1,5 +1,5 @@
 public class SC extends Veiculo {
-	
+	private static final int cil = 2500;
 	//private static final double fpiloto = 0.75;
 	//private static final double fcilindrada = 0.25;
 	
@@ -8,7 +8,7 @@ public class SC extends Veiculo {
 	}
 
 	public SC(String marca, String modelo, int cilindrada, int cv, Piloto p1, Piloto p2, int hib) {
-		super(marca,modelo,cilindrada,cv,p1,p2,0);
+		super(marca,modelo,cil,cv,p1,p2,0);
 	}
 	
 	public SC(SC s) {
