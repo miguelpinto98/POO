7f8af34a87b9cf6fca0542e014bf5ccfef9a3c97
Miguel Pinto <mcpinto98@gmail.com>
2013-05-17 16:49:23 +0100

    GT/SC new Class

---
+++ GT.java
@@ -0,0 +1,33 @@
+public class GT extends Veiculo{
+
+	public GT() {
+		super();
+	}
+	
+	public GT(String marca, String modelo, int cilindrada, int cv, Piloto p1, Piloto p2) {
+		super(marca,modelo,cilindrada,cv,p1,p2);
+	}
+	
+	public GT(GT g) {
+		super(g);
+	}
+	
+	/** Clone*/
+	public Veiculo clone() {
+		return new GT(this);
+	}
+	
+	/** Equals*/
+	public boolean equals(Object o) {
+		return (super.equals(o));
+	}
+	
+	/** ToString*/
+	public String toString() {
+        StringBuilder s = new StringBuilder ();
+        s.append("GT");
+        s.append(super.toString());
+        
+        return s.toString();
+	}
+}
---
+++ SC.java
@@ -0,0 +1,36 @@
+public class SC extends Veiculo {
+	
+	//private static final double fpiloto = 0.75;
+	//private static final double fcilindrada = 0.25;
+	
+	public SC() {
+		super();
+	}
+
+	public SC(String marca, String modelo, int cilindrada, int cv, Piloto p1, Piloto p2) {
+		super(marca,modelo,cilindrada,cv,p1,p2);
+	}
+	
+	public SC(SC s) {
+		super(s);
+	}
+	
+	/** Clone*/
+	public Veiculo clone() {
+		return new SC(this);
+	}
+	
+	/** Equals*/
+	public boolean equals(Object o) {
+		return (super.equals(o));
+	}
+	
+	/** ToString*/
+	public String toString() {
+        StringBuilder s = new StringBuilder ();
+        s.append("SC");
+        s.append(super.toString());
+        
+        return s.toString();
+	}
+}
