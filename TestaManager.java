ebf2f26164b21013ff29e3dcfc1b4cd7cac5bb50
Miguel Pinto <mcpinto98@gmail.com>
2013-05-29 03:50:53 +0100

    testes lixo!

--- TestaManager.java
+++ TestaManager.java
@@ -9,23 +9,23 @@ public class TestaManager {
 		Piloto p4 = new Piloto("Bom", "Reino Unido", 15, 8, false);
 		Piloto p5 = new Piloto("Mt bom", "França", 20, 10, false);
 
-		PC1 p11 = new PC1("Koenigsegg", "Agera R", 6000, 600, p1, p1, 0); // 960cv
-		PC1 p12 = new PC1("SSC", "Tuatara", 6000, 800, p2, p2, 0); // 1350cv
-		PC1 p13 = new PC1("SSC", "Ultimate Aero TT", 6000, 1000, p3, p3, 0); // 1280cv
-		PC1 p14 = new PC1("Bugatti", "Veyron", 6000, 1200, p4, p4, 0); // 1200cv
-		PC1 p15 = new PC1("Venom", "GT", 6000, 1350, p5, p5, 0); // 1261cv
+		PC1 p11 = new PC1Normal("Koenigsegg", "Agera R", 6000, 600, p1, p1, 0); // 960cv
+		PC1 p12 = new PC1Normal("SSC", "Tuatara", 6000, 800, p2, p2, 0); // 1350cv
+		PC1 p13 = new PC1Normal("SSC", "Ultimate Aero TT", 6000, 1000, p3, p3, 0); // 1280cv
+		PC1 p14 = new PC1Normal("Bugatti", "Veyron", 6000, 1200, p4, p4, 0); // 1200cv
+		PC1 p15 = new PC1Normal("Venom", "GT", 6000, 1350, p5, p5, 0); // 1261cv
 
-		PC2 p21 = new PC2("Ferrari Enzo", "", 4000, 450, p1, p1, 0);
-		PC2 p22 = new PC2("As Ascari A10", "", 4500, 650, p2, p2, 0);
-		PC2 p23 = new PC2("Aston Martin ", "", 5000, 700, p3, p3, 0);
-		PC2 p24 = new PC2("Edonis", "", 5500, 850, p4, p4, 0);
-		PC2 p25 = new PC2("Mercedes-Benz", "", 6000, 1000, p5, p5, 0);
+		PC2 p21 = new PC2Normal("Ferrari Enzo", "", 4000, 450, p1, p1, 0);
+		PC2 p22 = new PC2Normal("As Ascari A10", "", 4500, 650, p2, p2, 0);
+		PC2 p23 = new PC2Normal("Aston Martin ", "", 5000, 700, p3, p3, 0);
+		PC2 p24 = new PC2Normal("Edonis", "", 5500, 850, p4, p4, 0);
+		PC2 p25 = new PC2Normal("Mercedes-Benz", "", 6000, 1000, p5, p5, 0);
 
-		GT g1 = new GT("Peugeot 908 Equipe Peugeot", "", 3000, 200, p1, p1, 0);
-		GT g2 = new GT("Ferrari F10", "", 3400, 350, p2, p2, 0);
-		GT g3 = new GT("Mazda 787B Race Car", "", 3800, 500, p3, p3, 0);
-		GT g4 = new GT("Red Bull X2010 S.Vettel", "", 4100, 600, p4, p4, 0);
-		GT g5 = new GT("Hennessey Venom GT", "", 4500, 750, p5, p5, 0);
+		GT g1 = new GTNormal("Peugeot 908 Equipe Peugeot", "", 3000, 200, p1, p1, 0);
+		GT g2 = new GTNormal("Ferrari F10", "", 3400, 350, p2, p2, 0);
+		GT g3 = new GTNormal("Mazda 787B Race Car", "", 3800, 500, p3, p3, 0);
+		GT g4 = new GTNormal("Red Bull X2010 S.Vettel", "", 4100, 600, p4, p4, 0);
+		GT g5 = new GTNormal("Hennessey Venom GT", "", 4500, 750, p5, p5, 0);
 
 		SC s1 = new SC("Ferrari FXX", "", 2500, 100, p5, p1, 0);
 		SC s2 = new SC("Saleen S7", "", 2500, 200, p4, p2, 0);
