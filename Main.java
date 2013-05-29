58a47be40a71c085b2901c2e486aa6949d1c470c
Pedro Lima <pedrolima.0000@gmail.com>
2013-05-29 19:44:29 +0100

    main

---
+++ Main.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		
+		System.out.println("########## RACING MANAGER 2013 ##########");
+		System.out.println("1 - NOVO CAMPEONATO");
+		System.out.println("2 - CARREGAR CAMPEONATO");
+		System.out.println("3 - SAIR");
+		  System.out.println("             Escolha opção");
+		  System.out.println("#########################################");
+		   Scanner scanner = new Scanner(System.in);
+		   
+	     int x  = scanner.nextInt();
+	     
+	     switch(x){
+	     
+	     case 1 : break;
+	     case 2 : break;
+	     case 3 : break;
+	     
+	     
+	     
+	     
+	     }
+		
+	}
+}
