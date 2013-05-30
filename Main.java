a743055895ff34dcf27a7bc1fe54e927e8a82dbf
Pedro Lima <pedrolima.0000@gmail.com>
2013-05-30 18:03:49 +0100

    massacomarroz

--- Main.java
+++ Main.java
@@ -24,20 +24,6 @@ public class Main {
 		   	//case 1: c= geraCampeonato(); break;
 		   	case 2: ; break;
 		   	case 3: ; break;
-<<<<<<< HEAD
-		}		
-=======
-		   	}
-		   	
-		   	System.out.println("################## RACING MANAGER 2013 ##################");
-			System.out.println("#                                                       #");
-			System.out.println("#        1 - NOVO                             #");
-			System.out.println("#        2 - VOLTAR ATRÁS                               #");
-			System.out.println("#        3 - SAIR                                       #");
-			System.out.println("#                                                       #");
-			System.out.println("#        Escolha uma opção:                             #");
-			System.out.println("##################### Carregar Jogo #####################");
-	
->>>>>>> 707af5d34ebd147d605aad5b2a4b29b6d2a44fe6
+		}
 	}
 }
