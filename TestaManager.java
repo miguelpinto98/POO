538946c17b1178fef92470c2a20aea7e10177e71
Miguel Pinto <mcpinto98@gmail.com>
2013-06-01 03:07:46 +0100

    mais lixo

--- TestaManager.java
+++ TestaManager.java
@@ -3,35 +3,47 @@ import java.util.*;
 public class TestaManager {
 	public static void main(String[] args) throws Exception {
 
-		Piloto p1 = new Piloto("Muito Fraco", "China", 0, 2, false);
-		Piloto p2 = new Piloto("Fraco", "Portugal", 5, 4, false);
-		Piloto p3 = new Piloto("Razoavel", "Espanha", 10, 6, false);
-		Piloto p4 = new Piloto("Bom", "Reino Unido", 15, 8, false);
+		Piloto p1 = new Piloto("Muito Fraco", "China", 0, 10, false);
+		Piloto p2 = new Piloto("Fraco", "Portugal", 5, 10, false);
+		Piloto p3 = new Piloto("Razoavel", "Espanha", 10, 10, false);
+		Piloto p4 = new Piloto("Bom", "Reino Unido", 15, 10, false);
 		Piloto p5 = new Piloto("Mt bom", "França", 20, 10, false);
 
-		PC1 p11 = new PC1Normal("Koenigsegg", "Agera R", 6000, 600, p1, p1, 0); // 960cv
-		PC1 p12 = new PC1Normal("SSC", "Tuatara", 6000, 800, p2, p2, 0); // 1350cv
-		PC1 p13 = new PC1Normal("SSC", "Ultimate Aero TT", 6000, 1000, p3, p3, 0); // 1280cv
-		PC1 p14 = new PC1Normal("Bugatti", "Veyron", 6000, 1200, p4, p4, 0); // 1200cv
-		PC1 p15 = new PC1Normal("Venom", "GT", 6000, 1350, p5, p5, 0); // 1261cv
+		PC1 p11 = new PC1Normal("Koenigsegg", "Agera R", 6000, 600, p1, p1, 20); // 960cv
+		PC1 p12 = new PC1Normal("SSC", "Tuatara", 6000, 800, p2, p2, 75); // 1350cv
+		PC1 p13 = new PC1Normal("SSC", "Ultimate Aero TT", 6000, 1000, p3, p3, 100); // 1280cv
+		PC1 p14 = new PC1Normal("Bugatti", "Veyron", 6000, 1200, p4, p4, 150); // 1200cv
+		PC1 p15 = new PC1Normal("Venom", "GT", 6000, 1350, p5, p5, 200); // 1261cv
 		
-		PC1 p16 = new PC1Hibrido("Koenigsegg", "Agera R", 6000, 600, p1, p1, 0, 0); // 960cv
-		PC1 p17 = new PC1Hibrido("SSC", "Tuatara", 6000, 800, p2, p2, 0, 0); // 1350cv
-		PC1 p18 = new PC1Hibrido("SSC", "Ultimate Aero TT", 6000, 1000, p3, p3, 0, 0); // 1280cv
-		PC1 p19 = new PC1Hibrido("Bugatti", "Veyron", 6000, 1200, p4, p4, 0, 0); // 1200cv
-		PC1 p110 = new PC1Hibrido("Venom", "GT", 6000, 1350, p5, p5, 0, 0); // 1261cv
+		PC1 p16 = new PC1Hibrido("Koenigsegg", "Agera R", 6000, 600, p1, p1, 0, 40); // 960cv
+		PC1 p17 = new PC1Hibrido("SSC", "Tuatara", 6000, 800, p2, p2, 0, 75); // 1350cv
+		PC1 p18 = new PC1Hibrido("SSC", "Ultimate Aero TT", 6000, 1000, p3, p3, 0, 100); // 1280cv
+		PC1 p19 = new PC1Hibrido("Bugatti", "Veyron", 6000, 1200, p4, p4, 0, 150); // 1200cv
+		PC1 p110 = new PC1Hibrido("Venom", "GT", 6000, 1350, p5, p5, 0, 200); // 1261cv
 
 		PC2 p21 = new PC2Normal("Ferrari Enzo", "", 4000, 450, p1, p1, 0);
 		PC2 p22 = new PC2Normal("As Ascari A10", "", 4500, 650, p2, p2, 0);
 		PC2 p23 = new PC2Normal("Aston Martin ", "", 5000, 700, p3, p3, 0);
 		PC2 p24 = new PC2Normal("Edonis", "", 5500, 850, p4, p4, 0);
 		PC2 p25 = new PC2Normal("Mercedes-Benz", "", 6000, 1000, p5, p5, 0);
+		
+		PC2 p26 = new PC2Hibrido("Ferrari Enzo", "", 4000, 450, p1, p1, 0, 40);
+		PC2 p27 = new PC2Hibrido("As Ascari A10", "", 4500, 650, p2, p2, 0, 75);
+		PC2 p28 = new PC2Hibrido("Aston Martin ", "", 5000, 700, p3, p3, 0, 100);
+		PC2 p29 = new PC2Hibrido("Edonis", "", 5500, 850, p4, p4, 0, 150);
+		PC2 p210 = new PC2Hibrido("Mercedes-Benz", "", 6000, 1000, p5, p5, 0, 200);
 
 		GT g1 = new GTNormal("Peugeot 908 Equipe Peugeot", "", 3000, 200, p1, p1, 0);
 		GT g2 = new GTNormal("Ferrari F10", "", 3400, 350, p2, p2, 0);
 		GT g3 = new GTNormal("Mazda 787B Race Car", "", 3800, 500, p3, p3, 0);
 		GT g4 = new GTNormal("Red Bull X2010 S.Vettel", "", 4100, 600, p4, p4, 0);
 		GT g5 = new GTNormal("Hennessey Venom GT", "", 4500, 750, p5, p5, 0);
+		
+		GT g6 = new GTHibrido("Peugeot 908 Equipe Peugeot", "", 3000, 200, p1, p1, 0, 40);
+		GT g7 = new GTHibrido("Ferrari F10", "", 3400, 350, p2, p2, 0, 95);
+		GT g8 = new GTHibrido("Mazda 787B Race Car", "", 3800, 500, p3, p3, 0,130);
+		GT g9 = new GTHibrido("Red Bull X2010 S.Vettel", "", 4100, 600, p4, p4, 0,180);
+		GT g10 = new GTHibrido("Hennessey Venom GT", "", 4500, 750, p5, p5, 0,210);
 
 		SC s1 = new SC("Ferrari FXX", "", 2500, 100, p5, p1, 0);
 		SC s2 = new SC("Saleen S7", "", 2500, 200, p4, p2, 0);
@@ -65,11 +77,11 @@ public class TestaManager {
 		double tx1 = 0, tx2 = 0, tx3 = 0, tx4 = 0, tx5 = 0;
 		
 		  for(int i = 0 ; i < 5; i++){ double x1 =
-		  p11.tempoProximaVolta(ct1,false); double x2 =
-		  p12.tempoProximaVolta(ct1,false); double x3 =
-		  p13.tempoProximaVolta(ct1,false); double x4 =
-		  p14.tempoProximaVolta(ct1,false); double x5 =
-		  p15.tempoProximaVolta(ct1,false);
+		  g1.tempoProximaVolta(ct1,false); double x2 =
+		  g2.tempoProximaVolta(ct1,false); double x3 =
+		  g3.tempoProximaVolta(ct1,false); double x4 =
+		  g4.tempoProximaVolta(ct1,false); double x5 =
+		  g5.tempoProximaVolta(ct1,false);
 		  
 		  tx1+=x1; tx2+=x2; tx3+=x3; tx4+=x4; tx5+=x5;
 		  
@@ -78,12 +90,13 @@ public class TestaManager {
 		  System.out.println("\nP1 " + tx1/1000 + "\tP2 " + tx2/1000 + "\tP3 "
 		  + tx3/1000 + "\tP4 " + tx4/1000 + "\tP5 " + tx5/1000 + "\n");
 		  
-		 /* tx1=0; tx2=0; tx3=0; tx4=0; tx5=0; for(int i = 0 ; i < 5; i++){
-		 double x1 = p21.tempoProximaVolta(ct1,false); double x2 =
-		 p22.tempoProximaVolta(ct1,false); double x3 =
-		 p23.tempoProximaVolta(ct1,false); double x4 =
-		 p24.tempoProximaVolta(ct1,false); double x5 =
-		 p25.tempoProximaVolta(ct1,false);
+		  System.out.println("\nNORMAL");
+		 tx1=0; tx2=0; tx3=0; tx4=0; tx5=0; for(int i = 0 ; i < 5; i++){
+		 double x1 = g6.tempoProximaVolta(ct1,false); double x2 =
+		 g7.tempoProximaVolta(ct1,false); double x3 =
+		 g8.tempoProximaVolta(ct1,false); double x4 =
+		 g9.tempoProximaVolta(ct1,false); double x5 =
+		 g10.tempoProximaVolta(ct1,false);
 		 
 		 tx1+=x1; tx2+=x2; tx3+=x3; tx4+=x4; tx5+=x5;
 		 
